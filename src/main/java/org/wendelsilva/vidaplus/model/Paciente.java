f0bc7529f095e4aecdadb1db6e1613e3477f5cd0
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-08-18 22:30:02 -0300

    feat: add paciente model

---
+++ src/main/java/org/wendelsilva/vidaplus/model/Paciente.java
@@ -0,0 +1,30 @@
+package org.wendelsilva.vidaplus.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String password;
+    private String endereco;
+}
