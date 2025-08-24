590c93ed2f681732771fbd5d4aa57ce30affbb84
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-08-23 23:35:13 -0300

    feat: add consultastatus and consultatipo enums

---
+++ src/main/java/org/wendelsilva/vidaplus/model/ConsultaStatus.java
@@ -0,0 +1,9 @@
+package org.wendelsilva.vidaplus.model;
+
+public enum ConsultaStatus {
+    AGENDADA,
+    CONFIRMADA,
+    CANCELADA,
+    REALIZADA,
+    REMARCADA
+}
---
+++ src/main/java/org/wendelsilva/vidaplus/model/ConsultaTipo.java
@@ -0,0 +1,6 @@
+package org.wendelsilva.vidaplus.model;
+
+public enum ConsultaTipo {
+    PRESENCIAL,
+    ONLINE
+}
