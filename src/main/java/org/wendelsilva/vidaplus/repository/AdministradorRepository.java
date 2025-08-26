6281d4765058b04862732b448beec02a41e008f2
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-08-25 22:00:39 -0300

    feat: add administrador repository

---
+++ src/main/java/org/wendelsilva/vidaplus/repository/AdministradorRepository.java
@@ -0,0 +1,11 @@
+package org.wendelsilva.vidaplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.wendelsilva.vidaplus.model.Administrador;
+
+import java.util.UUID;
+
+@Repository
+public interface AdministradorRepository extends JpaRepository<Administrador, UUID> {
+}
