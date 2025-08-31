219b2806a1a46efb0b83177344c61ef5d1e38d3c
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-08-31 14:06:56 -0300

    feat: add method to find by email to repository

--- src/main/java/org/wendelsilva/vidaplus/repository/AdministradorRepository.java
+++ src/main/java/org/wendelsilva/vidaplus/repository/AdministradorRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.wendelsilva.vidaplus.model.Administrador;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, UUID> {
+    Optional<Administrador> findByEmail(String email);
 }
--- src/main/java/org/wendelsilva/vidaplus/repository/PacienteRepository.java
+++ src/main/java/org/wendelsilva/vidaplus/repository/PacienteRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.wendelsilva.vidaplus.model.Paciente;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, UUID> {
+    Optional<Paciente> findByEmail(String email);
 }
